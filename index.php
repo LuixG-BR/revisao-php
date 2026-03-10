5726ef32efb80a39b5a75d59ebb05c6e514952f7
LuixG-BR <luisgabriel19396@gmail.com>
2026-03-10 13:30:25 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Revisão PHP";
+
+$Nome = 'Luis';
+$nota = 7;
+
+if($nota >= 6){
+    echo "Aprovado";
+} else {
+    echo "Reprovado";
+}
+
+for($i; $i <= 3; $i++){
+    echo "Rodada $i";
+}
