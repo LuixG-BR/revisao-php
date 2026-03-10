80ffe3be9f6ec6cb2f3ab10db28e964bb5812cf6
LuixG-BR <luisgabriel19396@gmail.com>
2026-03-10 13:57:59 -0300

    add. switch

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+$valor = 10;
+switch($valor){
+    case 5:
+        echo "nota Vermelha"
+        break;
+    case 7:
+        echo "nota verde"
+        break;
+    case 10: 
+        echo "aprovado"
+        break;
+    default:
+    echo "Digite um valor valido"
+    break;
+}
